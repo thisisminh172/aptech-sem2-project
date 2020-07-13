61e43d86d33d8f32ab0a141004edc6805552096a
thisisminh172 <thisisminh172@gmail.com>
2020-07-13 10:32:58 +0700

    reset.css

--- inc/header.php
+++ inc/header.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css">
-    <script src="public/js/jquery-3.5.1.min.js"></script>
-    <script src="public/js/bootstrap/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="public/reset.css" type="text/css">
+    <script src="public/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    <script src="public/js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
     <div class="container-fluid">
