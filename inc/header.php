5ef374054cc5286bd51e7ecaeabc04ca052d13a0
thisisminh172 <thisisminh172@gmail.com>
2020-07-13 16:58:18 +0700

    db size check

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HAY GEAR</title>
     <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="public/reset.css" type="text/css">
     <link rel="stylesheet" href="public/style.css" type="text/css">
