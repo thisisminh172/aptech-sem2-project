cdb2f38f5a96c891038ca057fb18b0d8684098e4
thisisminh172 <thisisminh172@gmail.com>
2020-07-13 10:55:54 +0700

    global.css

--- inc/header.php
+++ inc/header.php
@@ -6,6 +6,9 @@
     <title>Document</title>
     <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="public/reset.css" type="text/css">
+    <link rel="stylesheet" href="public/style.css" type="text/css">
+
+
     <script src="public/js/jquery-3.5.1.min.js" type="text/javascript"></script>
     <script src="public/js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
 </head>
