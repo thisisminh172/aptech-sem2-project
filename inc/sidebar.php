93c03bfda23dc2f1dba19a538d9d71519bf44db1
thisisminh172 <thisisminh172@gmail.com>
2020-07-16 08:23:47 +0700

    new

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,11 +1,14 @@
-<div id="sidebar-wp" class="col-md-3 col-sm-12">
-    <ul id="sidebar">
-        <li><a href=""><i class="fas fa-headphones"></i> Tai nghe gaming</a></li>
-        <li><a href=""><i class="fas fa-keyboard"></i> Bàn phím cơ</a></li>
-        <li><a href=""><i class="fas fa-mouse"></i> Chuột gaming</a></li>
-        <li><a href=""><i class="fas fa-vr-cardboard"></i> VR</a></li>
-        <li><a href=""><i class="fas fa-chevron-right"></i> Combo phím chuột</a></li>
-        <li><a href=""><i class="fas fa-chevron-right"></i> Lót chuột</a></li>
-        <li><a href=""><i class="fas fa-chevron-right"></i> Combo</a></li>
-    </ul>
+<div id="sidebar-wp" class="col-md-2 col-sm-12">
+    <div class="container">
+        <ul id="sidebar">
+            <li><a href=""><i class="fas fa-headphones"></i> Tai nghe gaming</a></li>
+            <li><a href=""><i class="fas fa-keyboard"></i> Bàn phím cơ</a></li>
+            <li><a href=""><i class="fas fa-mouse"></i> Chuột gaming</a></li>
+            <li><a href=""><i class="fas fa-vr-cardboard"></i> VR</a></li>
+            <li><a href=""><i class="fas fa-chevron-right"></i> Combo phím chuột</a></li>
+            <li><a href=""><i class="fas fa-chevron-right"></i> Lót chuột</a></li>
+            <li><a href=""><i class="fas fa-chevron-right"></i> Combo</a></li>
+        </ul>
+    </div>
+
 </div>
